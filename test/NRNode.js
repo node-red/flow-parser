dd9bede7981b1cc44dea62f4df505aac47898746
Nick O'Leary <nick.oleary@gmail.com>
2021-07-12 21:01:58 +0100

    Add more tests to NRNode.js

--- test/NRNode.js
+++ test/NRNode.js
@@ -149,9 +149,124 @@ describe("NRNode", function() {
             const nodes = nodeMap['flow3-link1'].getSiblingNodes(true);
             validateNodeList(convertNodeArrayToMap(nodes), ['flow3-link2','flow3-node1'])
         });
-
-
     })
 
+    describe('getDownstreamNodes', function() {
+        it('no branches', function() {
+            const nodes = nodeMap['flow4-node2'].getDownstreamNodes();
+            validateNodeList(convertNodeArrayToMap(nodes), ['flow4-node3','flow4-node4','flow4-node5'])
+        })
+
+        it('branches on single output', function() {
+            const nodes = nodeMap['flow5-node2'].getDownstreamNodes();
+            validateNodeList(convertNodeArrayToMap(nodes), [
+                'flow5-node3',
+                'flow5-node4',
+                'flow5-node5',
+                'flow5-node8',
+                'flow5-node9'
+            ])
+        })
+
+        it('does not follow virtual links by default', function() {
+            const nodes = nodeMap['flow6-node1'].getDownstreamNodes();
+            validateNodeList(convertNodeArrayToMap(nodes), [
+                'flow6-node2',
+                'flow6-node3',
+                'flow6-link1'
+            ])
+        })
+
+        it('follows virtual links when asked', function() {
+            const nodes = nodeMap['flow6-node1'].getDownstreamNodes(true);
+            validateNodeList(convertNodeArrayToMap(nodes), [
+                'flow6-node2',
+                'flow6-node3',
+                'flow6-node4',
+                'flow6-link1',
+                'flow6-link2'
+            ])
+        })
+
+        it('does not get stuck in a loop', function() {
+            const nodes = nodeMap['flow7-node2'].getDownstreamNodes();
+            validateNodeList(convertNodeArrayToMap(nodes), [
+                'flow7-node3',
+                'flow7-node4'
+            ])
+        })
+    })
+
+    describe('getUpstreamNodes', function() {
+        it('no branches', function() {
+            const nodes = nodeMap['flow4-node4'].getUpstreamNodes();
+            validateNodeList(convertNodeArrayToMap(nodes), ['flow4-node1','flow4-node2','flow4-node3'])
+        })
+
+        it('branches on single input', function() {
+            const nodes = nodeMap['flow5-node4'].getUpstreamNodes();
+            validateNodeList(convertNodeArrayToMap(nodes), [
+                'flow5-node1',
+                'flow5-node2',
+                'flow5-node3',
+                'flow5-node6',
+                'flow5-node7'
+            ])
+        })
+        it('does not follow virtual links by default', function() {
+            const nodes = nodeMap['flow6-node4'].getUpstreamNodes();
+            validateNodeList(convertNodeArrayToMap(nodes), [
+                'flow6-link2'
+            ])
+        })
+
+        it('follows virtual links when asked', function() {
+            const nodes = nodeMap['flow6-node4'].getUpstreamNodes(true);
+            validateNodeList(convertNodeArrayToMap(nodes), [
+                'flow6-node1',
+                'flow6-node2',
+                'flow6-link1',
+                'flow6-link2'
+            ])
+        })
+
+        it('does not get stuck in a loop', function() {
+            const nodes = nodeMap['flow7-node3'].getUpstreamNodes();
+            validateNodeList(convertNodeArrayToMap(nodes), [
+                'flow7-node1',
+                'flow7-node2'
+            ])
+        })
+    })
+
+    describe('getConnectedNodes', function() {
+
+        it('does not follow virtual links by default', function() {
+            const nodes = nodeMap['flow6-link2'].getConnectedNodes();
+            validateNodeList(convertNodeArrayToMap(nodes), [
+                'flow6-node4'
+            ])
+        })
+
+        it('follows virtual links when asked', function() {
+            const nodes = nodeMap['flow6-link2'].getConnectedNodes(true);
+            validateNodeList(convertNodeArrayToMap(nodes), [
+                'flow6-node1',
+                'flow6-node2',
+                'flow6-node3',
+                'flow6-node4',
+                'flow6-link1'
+            ])
+        })
+
+        it('does not get stuck in a loop', function() {
+            const nodes = nodeMap['flow7-node2'].getConnectedNodes();
+            validateNodeList(convertNodeArrayToMap(nodes), [
+                'flow7-node1',
+                'flow7-node3',
+                'flow7-node4'
+            ])
+        })
+    })
 
 });
